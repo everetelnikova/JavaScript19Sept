b0898e1006cbd65b62c59eb384b361c50c756ea7
everetelnikova <zhenya_poka@mail.ru>
2020-02-14 20:42:55 +0600

    Практическая  работа № 14 задания 3.4.7.8.10 Практическая работа  №16 задания  11,12

--- Практическая работа 13/lab13.js
+++ Практическая работа 13/lab13.js
@@ -53,3 +53,62 @@
 		}
 	return result;	
 	}
+	
+	function Task5789(x,y,x1,y1)
+	{
+	let a = x1 - x;
+	let b = y1- y;
+	let result = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+	return result;	
+	}
+	
+	class Point{
+		constructor(x,y){
+		this.x = x;
+		this.y = y;		
+		}
+		distanceTo(u){
+			let p = u.x - this.x;
+			let q = u.y - this.y;
+			let result = Math.sqrt( Math.pow(p,2) + Math.pow(q,2))
+		return result;}
+	}
+	
+	function distanceBetweenObjects(a,b)
+	{
+	let result = Math.sqrt(Math.pow((b.x-a.x),2) + Math.pow((b.y-a.y),2))
+	return result;	
+	}
+	class Direction{
+		constructor(angle,quater,distance){
+			this.angle = angle;
+			this.quater= quater;
+			this.distance = distance;
+		}
+	} 
+    
+	function backwardTask(b,a){
+	let deltaX =a.x - b.x;
+	let deltaY =a.y - b.y;
+	let distance = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2))
+	let r = (Math.atan(Math.abs(deltaY/deltaX)))* 180 / Math.PI;
+	if (deltaX >= 0 && deltaY > 0){
+	return new Direction(r, 1, distance);
+	}
+		else if (deltaX < 0 && deltaY >= 0){
+		return new Direction(180-r, 2, distance);
+		}
+			else if (deltaX <= 0 && deltaY < 0){
+			return new Direction(180+r, 3, distance);			
+			}
+				else if(deltaX > 0 && deltaY <= 0){
+				return new Direction(360-r, 4, distance);
+				}
+
+
+	}
+	
+	
+	
+	
+	
--- Практическая работа 13/lab13spec.js
+++ Практическая работа 13/lab13spec.js
@@ -54,3 +54,176 @@ it("Ошибка", function()
 });
 
 });
+
+
+describe("Расстояние между точками", function()
+{
+
+it("Работает штатно", function()
+{
+	expect(Task5789(1,1,3,4)).toBeCloseTo(3.6056,4);
+});
+
+it("Всё еще работаююююю", function()
+{
+
+	expect(distanceBetweenObjects({x:1,y:1},{x:3,y:4})).toBeCloseTo(3.6056,4);
+});
+
+
+it("Всё еще работаююююю2", function()
+{
+	let a = {
+		x:1,
+		y:1,
+		distanceTo: function(x1,y1){
+			let p = x1 - this.x;
+			let q = y1 - this.y;
+			let result = Math.sqrt( Math.pow(p,2) + Math.pow(q,2))
+			return result;
+		}
+	}
+	expect(a.distanceTo(3,4)).toBeCloseTo(3.6056,4);
+});
+
+
+
+it("Тяжко, но я не сдаюсь", function()
+{
+	let b ={x:3,y:4}
+	let a = {
+		x:1,
+		y:1,
+		distanceTo: function(u){
+			let p = u.x - this.x;
+			let q = u.y - this.y;
+			let result = Math.sqrt( Math.pow(p,2) + Math.pow(q,2))
+			return result;
+		}
+	}
+	expect(a.distanceTo(b)).toBeCloseTo(3.6056,4);
+});
+
+it("Всё еще работаююююю, но теперь с классами", function()
+{
+	let a = new Point();
+	a.x = 1;
+	a.y = 1;
+	let b = new Point();
+	b.x = 3;
+	b.y = 4;
+	expect(distanceBetweenObjects(a,b)).toBeCloseTo(3.6056,4);
+});
+
+it("Всё еще работаююююю, но теперь с классами  и функция", function()
+{
+
+	let b = new Point();
+	b.x = 3;
+	b.y = 4;
+	let a = new Point();
+	a.x = 1;
+	a.y = 1;
+	let rslt = a.distanceTo(b);
+	expect(a.distanceTo(b)).toBeCloseTo(3.6056,4);
+	expect(b.distanceTo(a)).toBeCloseTo(3.6056,4);
+});
+
+it("Всё еще работаююююю, но теперь с классами  и функция КОНСТРУКТОРОМ", function()
+{
+
+	let b = new Point(3,4);
+	let a = new Point(1,1);
+	let rslt = a.distanceTo(b);
+	expect(a.distanceTo(b)).toBeCloseTo(3.6056,4);
+	expect(b.distanceTo(a)).toBeCloseTo(3.6056,4);
+});
+it("Всё еще работаююююю, но теперь с классами  и функция КОНСТРУКТОРОМ с 0", function()
+{
+
+	let b = new Point(0,0);
+	let a = new Point(0,0);
+	let rslt = a.distanceTo(b);
+	expect(a.distanceTo(b)).toBeCloseTo(0,4);
+	expect(b.distanceTo(a)).toBeCloseTo(0,4);
+});
+
+});
+
+describe("Обратная геодезическая задача", function()
+{
+it("Все ОК", function()
+{
+	let b = new Point(10,10);
+	let a = new Point(13,14);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(53.1301,4);
+	expect(current.distance).toBeCloseTo(5);
+	expect(current.quater).toBeCloseTo(1);
+});
+it("Все ОК1", function()
+{
+	let b = new Point(24,4);
+	let a = new Point(3,17);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(148.2405,4);
+	expect(current.distance).toBeCloseTo( 24.6982);
+	expect(current.quater).toBeCloseTo(2);
+});
+it("Все ОК2", function()
+{
+	let b = new Point(23,25);
+	let a = new Point(12,16);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(219.2894,4);
+	expect(current.distance).toBeCloseTo( 14.2127,4);
+	expect(current.quater).toBeCloseTo(3);
+});
+it("Все ОК3", function()
+{
+	let b = new Point(3,11);
+	let a = new Point(13,5);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(329.0362,4);
+	expect(current.distance).toBeCloseTo(11.6619,4);
+	expect(current.quater).toBeCloseTo(4);
+});
+it("Все ОК4", function()
+{
+	let b = new Point(5,5);
+	let a = new Point(5,10);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(90,4);
+	expect(current.distance).toBeCloseTo(5,4);
+	expect(current.quater).toBeCloseTo(1);
+});
+it("Все ОК5", function()
+{
+	let b = new Point(10,5);
+	let a = new Point(5,5);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(180,4);
+	expect(current.distance).toBeCloseTo(5,4);
+	expect(current.quater).toBeCloseTo(2);
+});
+it("Все ОК6", function()
+{
+	let b = new Point(5,10);
+	let a = new Point(5,5);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(270,4);
+	expect(current.distance).toBeCloseTo(5,4);
+	expect(current.quater).toBeCloseTo(3);
+});
+it("Все ОК7", function()
+{
+	let b = new Point(5,5);
+	let a = new Point(10,5);
+	current = backwardTask(b,a);
+	expect(current.angle).toBeCloseTo(360,4);
+	expect(current.distance).toBeCloseTo(5,4);
+	expect(current.quater).toBeCloseTo(4);
+});
+	
+});	
+
