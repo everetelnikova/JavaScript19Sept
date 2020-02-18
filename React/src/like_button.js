db5a3757fa2b09131b6ef019f5d033ed95ed52a8
everetelnikova <zhenya_polka@mail.ru>
2020-02-18 20:43:32 +0600

    Путешествие во времени

---
+++ React/src/like_button.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector('#btn_cont');
+ReactDOM.render(e(LikeButton), domContainer);
+
+const element = React.createElement('h1', null, 'Hello world');
+ ReactDOM.render(element, document.getElementById('root'));
